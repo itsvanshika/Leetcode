5c301ce79d23a74cad066075bd65f4bb53d6fc2b
Vanshika Gupta <115916095+itsvanshika@users.noreply.github.com>
2023-11-29 10:33:35 +0530

    Time: 4 ms (75.08%), Space: 23.3 MB (37.59%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums),end(nums));
+        for(int i=0;i<n-1;i++){
+            if(nums[i]!=i+1){
+                return false;
+            }
+        }
+        if(nums[n-1]!=n-1){
+            return false;
+        }
+        return true;
+        
+    }
+};
