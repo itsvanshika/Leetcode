a74abbfb3b6df7174bb54bce17164214facde5f4
Vanshika Gupta <115916095+itsvanshika@users.noreply.github.com>
2023-11-29 09:26:39 +0530

    Time: 40 ms (28.68%), Space: 13.6 MB (18.61%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    
+    def shuffle(self, nums,n):
+        for i in range(n):
+            nums[i] = nums[i]<<10
+            nums[i] = nums[i] | nums[i+n]
+        j = 2*n-1;
+        for i in range(n-1,-1,-1):
+            y = nums[i] & (2**10-1)
+            x = nums[i] >> 10
+            nums[j] = y
+            nums[j-1] = x
+            j=j-2
+        return nums
+       
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: List[int]
+        """
+        
