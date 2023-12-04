0b20c4d0d6c710ac87cb5f6569b04e1737a2beb2
Vanshika Gupta <115916095+itsvanshika@users.noreply.github.com>
2023-12-04 10:51:09 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (9.23%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,12 +1,30 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-        int ans = n;
-        ans ^= ans >> 16;
-        ans ^= ans >> 8;
-        ans ^= ans >> 4;
-        ans ^= ans >> 2;
-        ans ^= ans >> 1;
-        return ans;
+        if(n==0){
+            return 0;
+        }
+        vector<long long> f(31,0);
+        f[0] =1;
+        for(int i=1;i<=30;i++){
+            f[i] = 2*f[i-1] + 1;
+        }
+        int result =0;
+        int sign = 1;
+        for(int i=30;i>=0;i--){
+            int ith_bit = ((1 << i) & n);
+            if(ith_bit == 0){
+                continue;
+            }
+            if(sign > 0){
+                result+= f[i];
+            }
+            else{
+                result -= f[i];
+            }
+            sign = sign * (-1);
+        }
+        return result;
+        
     }
 };
