795ad24eed654c5493af320a415c30d99a522362
Vanshika Gupta <115916095+itsvanshika@users.noreply.github.com>
2024-01-30 08:27:22 +0530

    Time: 429 ms (68.24%), Space: 154.1 MB (63.04%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(); //m
-        int cols = matrix[0].size(); //n
-        
-        //First taje the cumulative sum row-wise
-        for(int row = 0; row<rows; row++) {
-            for(int col = 1; col<cols; col++) {
-                matrix[row][col] += matrix[row][col-1];
+        int row = matrix.size();
+        int col = matrix[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=1;j<col;j++){
+                matrix[i][j]=matrix[i][j]+matrix[i][j-1];
             }
         }
-        
-        //Now, you need to find the "No. of subarrays with sum k" in downward direction
-        int result = 0;
-        for(int startCol = 0; startCol<cols; startCol++) {
-            
-            for(int currCol = startCol; currCol<cols; currCol++) {
-                //We need to find all sub matrices sum
-                
-                //Now comes the concept of "No. of subarrays with sum k"
-                unordered_map<int, int> mp;
-                mp[0] = 1;
-                int sum = 0;
-                //Go downwards row wise
-                for(int row = 0; row<rows; row++) {
-                    sum += matrix[row][currCol] - (startCol > 0 ? matrix[row][startCol-1] : 0);
-                    
-                    if(mp.count(sum-target)) {
-                        result += mp[sum-target];
-                    }
-                    
-                    mp[sum]++;
-                    
-                }
-                
-            }
+        int result=0;
+        for(int i=0;i<col;i++){
+           for(int j=i;j<col;j++){
+               unordered_map<int,int>mp;
+               mp[0]=1;
+               int currsum=0;
+               for(int k=0;k<row;k++){
+                   currsum+=matrix[k][j]-(i>0?matrix[k][i-1]:0);
+                   if(mp.find(currsum-target)!=mp.end()){
+                       result+=mp[currsum-target];
+                   }
+                   mp[currsum]++;
+               }
+           } 
         }
-        
         return result;
     }
 };
