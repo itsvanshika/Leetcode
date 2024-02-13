086c0c8c8fc25f9a41ae03eb09ea97dc376815e6
Vanshika Gupta <115916095+itsvanshika@users.noreply.github.com>
2024-02-13 14:10:18 +0530

    Time: 20 ms (35.85%), Space: 9.3 MB (52.32%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,25 +1,27 @@
-class Solution 
-{
-private:
-    string t;
-    int check(int l, int r, int ans = 0)
-    {
-        while(l >= 0 and r <= t.size())
-        {
-            if(t[l--] == t[r++]) ans++;
-            else break;
-        }
-        return ans;
-    }
+class Solution {
 public:
-    int countSubstrings(string s) 
-    {
-        int n = size(s), ans = 0;t = s;
-        for(int i = 0; i < n; i++)
-        {
-            ans += check(i,i);    // odd length palindromes
-            ans += check(i,i+1); // even length palindromes
+   
+    int countSubstrings(string s) {
+        int n =  s.length();
+        int count=0;
+        vector<vector<bool>>t(n,vector<bool>(n,false));
+        for(int l=1;l<=n;l++){
+            for(int i=0;l+i-1<n;i++){
+                int j =  l+i-1;
+                if(i==j){
+                    t[i][j]=true;
+                }
+                else if(i+1==j){
+                    t[i][j]=(s[i]==s[j]);
+                }
+                else{
+                    t[i][j] = ((s[i]==s[j]) && t[i+1][j-1]);
+                }
+                if(t[i][j]){
+                    count++;
+                }
+            }
         }
-        return ans;
+        return count;
     }
 };
