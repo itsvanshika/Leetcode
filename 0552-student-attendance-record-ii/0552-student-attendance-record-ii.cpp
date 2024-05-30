7628ecc5cd9e60d8f96f09e86903da7d13ae022c
Vanshika Gupta <115916095+itsvanshika@users.noreply.github.com>
2024-05-30 19:20:48 +0530

    Time: 207 ms (58.99%), Space: 17.5 MB (80.01%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.cpp
@@ -1,41 +1,24 @@
 class Solution {
-    int MOD = 1000000007;
-    // Cache to store sub-problem results.
-    vector<vector<vector<int>>> memo;
-
 public:
-    // Recursive function to return the count of combinations of length 'n' eligible for the award.
-    int eligibleCombinations(int n, int totalAbsences, int consecutiveLates) {
-        // If the combination has become not eligible for the award,
-        // then we will not count any combinations that can be made using it.
-        if (totalAbsences >= 2 or consecutiveLates >= 3) {
+    int mod = 1e9+7;
+    int t[100001][2][3];
+    int solve(int n,int absent,int late){
+        if(absent>=2 || late>=3){
             return 0;
         }
-        // If we have generated a combination of length 'n' we will count it.
-        if (n == 0) {
+        if(n==0){
             return 1;
         }
-        // If we have already seen this sub-problem earlier, we return the stored result.
-        if (memo[n][totalAbsences][consecutiveLates] != -1) {
-            return memo[n][totalAbsences][consecutiveLates];
+        if(t[n][absent][late]!=-1){
+            return t[n][absent][late];
         }
-
-        int count = 0;
-        // We choose 'P' for the current position.
-        count = eligibleCombinations(n - 1, totalAbsences, 0);
-        // We choose 'A' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences + 1, 0)) % MOD;
-        // We choose 'L' for the current position.
-        count = (count + eligibleCombinations(n - 1, totalAbsences, consecutiveLates + 1)) % MOD;
-
-        // Return and store the current sub-problem result in the cache.
-        return memo[n][totalAbsences][consecutiveLates] = count;
+        int l = solve(n-1,absent,late+1) %mod;
+        int a = solve(n-1,absent+1,0)%mod;
+        int p = solve(n-1,absent,0)%mod;
+        return t[n][absent][late] = ((l+a)%mod +p)%mod;
     }
-
     int checkRecord(int n) {
-        // Initialize the cache.
-        memo = vector<vector<vector<int>>>(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
-        // Return count of combinations of length 'n' eligible for the award.
-        return eligibleCombinations(n, 0, 0);
+        memset(t,-1,sizeof(t));
+        return solve(n,0,0);
     }
 };
