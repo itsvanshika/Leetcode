19ce263144f1321e96c2f7a963121bb7ad903c0a
Vanshika Gupta <115916095+itsvanshika@users.noreply.github.com>
2024-07-12 23:00:19 +0530

    Time: 123 ms (39.36%), Space: 62.3 MB (40.17%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,39 +1,39 @@
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
 class Solution {
 public:
+    typedef pair<int, int> p;
+    
     int longestSubarray(vector<int>& nums, int limit) {
-        priority_queue<pair<int, int>> maxHeap;
-        priority_queue<pair<int, int>, vector<pair<int, int>>,
-                       greater<pair<int, int>>>
-            minHeap;
-
-        int left = 0, maxLength = 0;
-
-        for (int right = 0; right < nums.size(); ++right) {
-            maxHeap.push({nums[right], right});
-            minHeap.push({nums[right], right});
-
-            // Check if the absolute difference between the maximum and minimum
-            // values in the current window exceeds the limit
-            while (maxHeap.top().first - minHeap.top().first > limit) {
-                // Move the left pointer to the right until the condition is
-                // satisfied. This ensures we remove the element causing the
-                // violation
-                left = min(maxHeap.top().second, minHeap.top().second) + 1;
-
-                // Remove elements from the heaps that are outside the current
-                // window
-                while (maxHeap.top().second < left) {
-                    maxHeap.pop();
+        int n = nums.size();
+        int maxlength = 0;
+        priority_queue<p, vector<p>> maxpq;
+        priority_queue<p, vector<p>, greater<p>> minpq;
+        int i = 0;
+        int j = 0;
+        
+        while (j < n) {
+            maxpq.push({nums[j], j});
+            minpq.push({nums[j], j});
+            
+            while (maxpq.top().first - minpq.top().first > limit) {
+                i = min(maxpq.top().second, minpq.top().second) + 1;
+                while (!maxpq.empty() && maxpq.top().second < i) {
+                    maxpq.pop();
                 }
-                while (minHeap.top().second < left) {
-                    minHeap.pop();
+                while (!minpq.empty() && minpq.top().second < i) {
+                    minpq.pop();
                 }
             }
-
-            // Update maxLength with the length of the current valid window
-            maxLength = max(maxLength, right - left + 1);
+            
+            maxlength = max(maxlength, j - i + 1);
+            ++j;
         }
-
-        return maxLength;
+        
+        return maxlength;
     }
 };
